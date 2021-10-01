31f47e6138a92d178a627c2ca77d3189ca89660b
DESKTOP-RUP4QFC\sergi <sergio.rdlv@gmail.com>
2021-10-01 22:42:12 +0200

    Implementación de Ranking final

--- classes/Ticket.py
+++ classes/Ticket.py
@@ -1,8 +1,9 @@
 class Ticket:
-    def __init__(self, id, colour, numbers):
+    def __init__(self, id, colour, numbers, rank):
         self.id = id
         self.colour = colour
         self.numbers = []
+        self.rank = rank
         
         for number in numbers.split(","):
             self.numbers.append(number)
@@ -11,6 +12,10 @@ class Ticket:
         string = "\n"
         string += "id: " + str(self.id) + "\n"
         string += "colour: " + self.colour + "\n"
+        string += "rank: " + str(self.rank) + "\n"
         for number in self.numbers:
             string += str(number) + " "
         return string
+    
+    def update_rank(self, rank):
+        self.rank = rank
